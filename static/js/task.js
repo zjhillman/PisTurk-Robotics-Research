042d5930852722ef1fd455a6d90e2ff86916f5a4
Zachary Hillman <zjhillman@outlook.com>
2023-02-22 23:32:35 -0600

    changed timer to 30 seconds, unable to get <video> duration fromm stage.html in task.js

--- static/js/task.js
+++ static/js/task.js
@@ -64,8 +64,8 @@ var HriTest = function() {
 	// enable next button when video ends
 
 	async function buttonUpdate() {
-		console.log("Waiting for "+"3"+" seconds");
-		await new Promise(r => setTimeout(r, 3000));
+		console.log("Waiting for "+"30"+" seconds");
+		await new Promise(r => setTimeout(r, 30000));
 		$('#next').removeAttr('disabled');
 	}
 
